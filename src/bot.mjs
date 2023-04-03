221aa054cfd1d13300e833f7c97df31be1991ab4
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-04 02:20:17 +0500

    Added methods

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,29 @@
-import TeleBot from "telebot"
+import {parseCommands, NewMethodsMixin} from "telebot-utils";
+import {circle} from "./svg.mjs";
+import TeleBot from "telebot";
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const {TELEGRAM_BOT_TOKEN} = process.env;
 
-bot.on("text", msg => msg.reply.text(msg.text))
+class ColormojiBot extends NewMethodsMixin(TeleBot) {
+    constructor(...args) {
+        super(...args);
+        this.mod("message", parseCommands);
+        this.on("text", this.text.bind(this));
+    }
 
-export default bot
+    async text(message = {}) {
+        const {isCommand, text, from: {id} = {}, reply = {}} = message || {};
+        if (isCommand) return this.command(message);
+        return reply.text(circle(text));
+    }
+
+    async command(message = {}) {
+        const {command, text, from: {id} = {}, reply = {}} = message || {};
+        switch (command) {
+            default:
+                return reply.text("Send any color in HEX to make Emoji");
+        }
+    }
+}
+
+export default new ColormojiBot(TELEGRAM_BOT_TOKEN);
